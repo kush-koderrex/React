b6d229e9003b3565e47b25e55151e5c13eaf3477
Kush <kushkumar89792666@gmail.com>
2022-05-24 22:16:03 +0530

    dark mode

--- textutils/src/App.js
+++ textutils/src/App.js
@@ -1,16 +1,29 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 import Navbar from './components/Navbar';
-// import TextForm from './components/TextForm';
+import TextForm from './components/TextForm';
 import About from './components/About';
+import React, { useState } from 'react';
 
 function App() {
+  const [mode, setMode] = useState("dark");
+  const toggleMode = ()=>{
+    if(mode === "light"){
+      setMode("dark");
+      document.body.style.backgroundColor='black'
+    }else{
+      setMode("light");
+      document.body.style.backgroundColor='white'
+
+    }
+
+  } 
   return (
     <>
-    <Navbar title = "kush"/>
+    <Navbar title = "kush" mode={mode} toggleMode = {toggleMode}/>
     <div className="container my-3" >
-    {/* <TextForm heading= "Enter Text"/> */}
-    <About/>
+    <TextForm heading= "Enter Text"/>
+    {/* <About/> */}
     </div>
   
     </>
--- textutils/src/components/TextForm.js
+++ textutils/src/components/TextForm.js
@@ -15,20 +15,21 @@ export default function TextForm(props) {
   const [text,setText] = useState("Enter Text Here");
   return (
     <>
-    <div className="container">
+    <div className="container "style= {{color:props.mode==='dark'?'black':'white'}}>
       <h1>{props.heading}</h1>
       <div className="mb-3">
         <textarea
           className="form-control"
           value={text}
+          style= {{backgroundColor:props.mode==='dark'?'black':'white'}}
           onChange={handleonchange}
           id="mybox"
           rows="3"
         ></textarea>
       </div>
-      <button className="btn btn-primary" onClick={handleClick}>Convert To uppercase</button>
+      <button className="btn btn-primary" onClick={handleClick} >Convert To uppercase</button>
     </div>
-    <div className="container my -2">
+    <div className="container my -2"style= {{color:props.mode==='dark'?'black':'white'}}>
       <h1>Summary</h1>
       <p>{text.split(" ").length} words and {text.length} sentences</p>
       <p>{0.008*text.split(" ").length} Time</p>
